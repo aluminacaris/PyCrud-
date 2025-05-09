944b7b1fb5cc1d3cbb7a23d945e0145a68b66c71
aluminacaris <mmaibuk@outlook.com>
2025-05-09 12:52:18 -0300

    inserida função read, com SELECT no BD

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     connection = conn()
     cursor = connection.cursor()
 
+    # CREATE
     def insert_obra(ID_obra, titulo, ano_public, edicao, num_paginas, lingua, autor):
         cmd_insert = "INSERT INTO obras(ID_obra, titulo, ano_public, edicao, num_paginas, lingua, autor) VALUES (%s,%s,%s,%s,%s,%s,%s); "
         values = ID_obra, titulo, ano_public, edicao, num_paginas, lingua, autor
@@ -12,6 +13,16 @@ def main():
         connection.commit()
         print('dados inseridos com sucesso!')
 
+    # READ
+    def seleciona():
+        cmd_select = "SELECT ID_obra, titulo, ano_public, edicao, num_paginas, lingua, autor FROM obras;"
+        cursor.execute(cmd_select)
+        acoes = cursor.fetchall()
+        for acao in acoes:
+            print(acao)
+        return acao
+
+    # UPDATE
     encerra_conn(connection)
 
 
