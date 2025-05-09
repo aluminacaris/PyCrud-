8eadd477f4d139ec3dd574f0e5c2d5be94cf5133
aluminacaris <mmaibuk@outlook.com>
2025-05-09 12:45:57 -0300

    inserida função create, com INSERT no BD

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from connect import conn, encerra_conn
 def main():
 
     connection = conn()
+    cursor = connection.cursor()
+
+    def insert_obra(ID_obra, titulo, ano_public, edicao, num_paginas, lingua, autor):
+        cmd_insert = "INSERT INTO obras(ID_obra, titulo, ano_public, edicao, num_paginas, lingua, autor) VALUES (%s,%s,%s,%s,%s,%s,%s); "
+        values = ID_obra, titulo, ano_public, edicao, num_paginas, lingua, autor
+        cursor.execute(cmd_insert, values)
+        connection.commit()
+        print('dados inseridos com sucesso!')
 
     encerra_conn(connection)
 
